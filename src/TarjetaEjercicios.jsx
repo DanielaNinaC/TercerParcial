2788bd69206661d933e0775cc8bba76ba7703ce0
Daniela Nina <danielaninacastillo@gmail.com>
2024-06-20 12:17:39 -0400

    Componenete Tarjeta ejercicios

--- src/TarjetaEjercicios.jsx
+++ src/TarjetaEjercicios.jsx
@@ -1,12 +1,18 @@
 import "./TarjetaEjercicios.css"
 import DescripcionEjer from "./DescripcionEjer";
 import Ejer from "./assets/ejer.jpg"
+import Flecha from "./assets/Vector.png"
 function TarjetaEjercicios(){
     return(
         <>
         <div className="ContenedorTarjeta">
             <img className="ImagenEjer" src={Ejer} alt="" />
-            <DescripcionEjer/>
+
+            <div className="ContenedorDes">
+                <DescripcionEjer/>
+            </div>
+
+            <img className="ImgFlecha" src={Flecha}alt="" />
         </div>
         </>
 
