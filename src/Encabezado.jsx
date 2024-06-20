b006a939f86d9736b5763c596d2c15e29e38fd6e
Daniela Nina <danielaninacastillo@gmail.com>
2024-06-20 11:43:12 -0400

    Encabezado

---
+++ src/Encabezado.jsx
@@ -0,0 +1,14 @@
+import "./Encabezado.css"
+
+function Encabezado (){
+    return (
+        <div className="ContenedorEncabezado">
+            <h1 className="Encabezado">
+                Ejercicios  4
+            </h1>
+            
+        </div>
+
+    )
+}
+export default Encabezado;
